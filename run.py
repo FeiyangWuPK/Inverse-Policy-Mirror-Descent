2c6c3c49f08d096e28748aa7d6d3d5c35956d793
WU Feiyang <patrickfywu@icloud.com>
2022-08-30 16:21:39 -0400

    update to apmd

--- run.py
+++ run.py
@@ -1,16 +1,15 @@
-from mdpo_on.mdpo import MDPO
+from apmd_on.apmd import APMD
 import numpy as np
 
 import gym
 import torch as th
-from mdpo_on.policy import MDPOPolicy
 from stable_baselines3.sac.policies import SACPolicy
 from stable_baselines3 import SAC
 
 env = gym.make('CartPole-v1')
 policy_kwargs = dict(activation_fn=th.nn.ReLU,
                      net_arch=[dict(pi=[32, 32], vf=[32, 32])])
-model = MDPO("MlpPolicy", env, verbose=1)
+model = APMD("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=200000)
 
 obs = env.reset()
